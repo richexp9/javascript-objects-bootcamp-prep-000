ad6d24b46e938d4ad8f56bd6ba7d98439d29089e
Richard Loera <richexp9@gmx.com>
2017-12-04 19:31:00 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,5 @@
 var playlist = {
+<<<<<<< HEAD
   Kanye: "Gold Digger"
 }
 
@@ -10,4 +11,16 @@ function updatePlaylist(playlist, artistName, songTitle) {
 function removeFromPlaylist(playlist, artist) {
   delete playlist.Kanye
   return playlist
+=======
+  'Phil Ochs': "Here's to the State of Mississippi",
+}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist.Kanye = "Gold Digger"
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistNameS) {
+
+>>>>>>> c25cf743403677a91e997b1637c07ffd654a7059
 }
